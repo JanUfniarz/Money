e3d7f6f725fdb24673f22c970922feee85e1ccb3
Jan Ufniarz <jan.ufniarz@gmail.com>
2023-03-13 17:18:49 +0100

    Money project

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp());
+
+class View extends StatefulWidget {
+
+  @override
+  State<View> createState() => _ViewState();
+}
+
+class _ViewState extends State<View> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+
