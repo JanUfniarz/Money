c8366ac3fb57ddd18b01db94d83d0ab5bb6ba905
Jan Ufniarz <jan.ufniarz@gmail.com>
2023-03-15 17:52:11 +0100

    balance_card funcionality

--- android/app/src/main/java/com/example/money/MainActivity.java
+++ android/app/src/main/java/com/example/money/MainActivity.java
@@ -14,13 +14,17 @@ import java.util.List;
 import java.util.Map;
 
 public class MainActivity extends FlutterActivity {
-    private static final String CHANNEL = "com.flutter.epic/main";
+    private static final String CHANNEL = "com.flutter.balance_card/MainActivity";
 
-    private List<Account> accounts = new ArrayList<>();
+    private final List<Account> accounts = new ArrayList<>();
 
     @RequiresApi(api = Build.VERSION_CODES.TIRAMISU)
     @Override
     public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+
+        accounts.add(new Account("Test Acc", 1250.75));
+        accounts.add(new Account("Test 2", 124.50));
+
         super.configureFlutterEngine(flutterEngine);
         new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL)
                 .setMethodCallHandler(
@@ -28,9 +32,11 @@ public class MainActivity extends FlutterActivity {
                             final Map<String, Object> arguments = call.arguments();
 
                             switch (call.method) {
+
                                 default:
                                     result.notImplemented();
                                     break;
+
                                 case "addAccount":
                                     //result.success(addAccount());
                                     accounts.add(new Account(
@@ -38,13 +44,45 @@ public class MainActivity extends FlutterActivity {
                                             (double) arguments.get("value")
                                     ));
                                     break;
+
                                 case "balanceSum":
                                     double sum = accounts.stream()
                                             .map(account -> account.value)
                                             .reduce(Double::sum).orElseThrow();
                                     String res = String.valueOf(sum);
                                     result.success(res);
+                                    break;
 
+                                case "getName":
+                                    try {
+                                        String name = ((Account) accounts
+                                                .get((int) arguments.get("index")))
+                                                .name;
+                                        result.success(name);
+                                    } catch (IndexOutOfBoundsException e) {
+                                        result.error(
+                                                "IOOBE",
+                                                "IndexOutOfBoundsException",
+                                                "");
+                                    }
+                                    break;
+                                case "getValue":
+                                    try {
+                                        double value = ((Account) accounts
+                                                .get((int) arguments.get("index")))
+                                                .value;
+                                        result.success(value);
+                                    } catch (IndexOutOfBoundsException e) {
+                                        result.error(
+                                                "IOOBE",
+                                                "IndexOutOfBoundsException",
+                                                "");
+                                    }
+                                    break;
+                                case "getLength":
+                                    int len = accounts.size();
+                                    result.success(len);
+                                    break;
                             }
                         }
                 );
--- lib/balance_card.dart
+++ lib/balance_card.dart
@@ -1,51 +1,81 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:money/my_color.dart';
 
 class BalanceCard extends StatefulWidget {
 
   @override
   State<BalanceCard> createState() => _BalanceCardState();
 
-  BalanceCard();
+  BalanceCard({super.key});
 }
 
 class _BalanceCardState extends State<BalanceCard> {
 
+  int accountCount = 0;
+  List<Widget> accountCards = [];
+
+  @override
+  void initState() {
+    super.initState();
+    getLength();
+  }
+
   @override
   Widget build(BuildContext context) {
+
     return Card(
       color: Colors.grey[850],
       child: Column(
         children: <Widget>[
-          FutureBuilder<String>(
-            future: balanceSum(),
-            builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
-                if (snapshot.hasData) {
-                  return Text(snapshot.data ?? "0.00");
-                } else {
-                  return CircularProgressIndicator();
-                }
-              },
-          ),
-          ElevatedButton(
-            onPressed: () =>
-                setState(() =>
-                    addAccount(
-                      name: "account",
-                      value: 100.50,
-                    )
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text(
+                "  Your\n  Balance: ",
+                style: TextStyle(
+                    color: MyColor.greenLight,
+                    fontSize: 20
                 ),
-            child: Icon(Icons.accessibility),
-          )
+              ),
+              //SizedBox(width: 10),
+              FutureBuilder<String>(
+                future: balanceSum(),
+                builder: (
+                    BuildContext context,
+                    AsyncSnapshot<String> snapshot
+                    ) {
+                    if (snapshot.hasData) {
+                      return Text(
+                        snapshot.data ?? "0.00",
+                        style: TextStyle(
+                          color: MyColor.greenLight,
+                          fontSize: 50
+                        ),
+                      );
+                    } else {
+                      return CircularProgressIndicator();
+                    }
+                  },
+              ),
+              SizedBox(width: 80),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: accountCards,
+          ),
         ],
       ),
     );
   }
 
-  static const channel = MethodChannel("com.flutter.epic/main");
+  static const channel = MethodChannel("com.flutter.balance_card/MainActivity");
 
   addAccount({required String name, required double value}) {
-    var arguments = <String, dynamic>{
+    var arguments = <String, dynamic> {
       "name": name,
       "value": value,
     };
@@ -60,5 +90,111 @@ class _BalanceCardState extends State<BalanceCard> {
       print(e);
     }
     return res;
+  }
+
+  void getLength() async {
+    accountCount = await channel.invokeMethod("getLength");
+    _buildAccountCards();
+  }
+
+  List<Widget> _upList() {
+    List<Widget> res = [];
+    int index = 0;
+    while (index < accountCount) {
+      res.add(AccountCard(index: index));
+      print("res: $res");
+      index++;
+    }
+    return res;
+  }
+
+  void _buildAccountCards() {
+    setState(() {
+      accountCards = _upList();
+    });
+  }
+
+
+}
+
+class AccountCard extends StatefulWidget {
+  static const channel = MethodChannel("com.flutter.balance_card/MainActivity");
+  int index;
+
+
+  AccountCard({super.key, required this.index});
+
+  @override
+  State<AccountCard> createState() => _AccountCardState();
+}
+
+class _AccountCardState extends State<AccountCard> {
+
+  Future<String> name() async {
+    String name = "";
+    var arguments = <String, dynamic>{
+      "index" : widget.index
+    };
+    name = await AccountCard.channel.invokeMethod("getName", arguments);
+    return name;
+  }
+
+  Future<String> value() async {
+    double value = 0.00;
+    var arguments = <String, dynamic>{
+      "index" : widget.index
+    };
+    value = await AccountCard.channel.invokeMethod("getValue", arguments);
+    return value.toString();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: MyColor.greenDark,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          FutureBuilder<String>(
+            future: name(),
+            builder: (
+                BuildContext context,
+                AsyncSnapshot<String> snapshot
+                ) {
+              if (snapshot.hasData) {
+                return Text(
+                  snapshot.data ?? "null",
+                  style: TextStyle(
+                      color: MyColor.greenLight,
+                      fontSize: 30
+                  ),
+                );
+              } else {
+                return CircularProgressIndicator();
+              }
+            },
+          ),
+          FutureBuilder<String>(
+            future: value(),
+            builder: (
+                BuildContext context,
+                AsyncSnapshot<String> snapshot
+                ) {
+              if (snapshot.hasData) {
+                return  Text(
+                  snapshot.data ?? "null",
+                  style: TextStyle(
+                      color: MyColor.greenLight,
+                      fontSize: 30
+                  ),
+                );
+              } else {
+                return CircularProgressIndicator();
+              }
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
